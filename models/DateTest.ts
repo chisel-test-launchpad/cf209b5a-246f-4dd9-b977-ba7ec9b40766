76ba59b87017314fca27664f57018b9d6fea70f9
chiselnow <chiselnow@chiselstrike.com>
2022-11-07 17:00:17 -0300

    Update Launchpad 9f347754-1b81-423c-8c89-95f28ff8500e

---
+++ models/DateTest.ts
@@ -0,0 +1,5 @@
+import { ChiselEntity } from "@chiselstrike/api";
+export class DateTest extends ChiselEntity {
+    text: string;
+    createdAt: Date;
+}
---
+++ routes/date-tests.ts
@@ -0,0 +1,3 @@
+import { crud } from '@chiselstrike/api';
+import { DateTest } from '../models/DateTest';
+export default crud(DateTest);
